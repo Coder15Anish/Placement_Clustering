e68a21557e7483044179d07122cb5c48b13a1b46
Coder15Anish <108470695+Coder15Anish@users.noreply.github.com>
2025-06-11 23:39:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import pandas as pd
-import numpy as np
 import streamlit as st
-from sklearn.preprocessing import LabelEncoder,StandardScaler
-from sklearn.cluster import KMeans
-from sklearn.decomposition import PCA
+from cluster import create_cluster,calculate_iq,plot
+import time
 
 flag=False
 features = ['CGPA', 'Internships', 'Projects', 'Workshops/Certifications',
@@ -14,55 +12,51 @@ st.title("🎓 Student Placement Clustering App (K-Means)")
 st.write("Upload a dataset to cluster students into High, Medium, or Low placement chance groups.")
 
 # File uploader
-df = st.file_uploader("Upload your CSV file", type=["csv"])
+df = st.file_uploader("Upload your CSV file", type=["csv"])  
+
+
 
 if df is not None:
     df = pd.read_csv(df)
 
 if df is not None:
-    
-
     try:
-            df_encoded=df[features]
-            cat_col=df_encoded.select_dtypes(include=['object']).columns
-            label=LabelEncoder()
-            for col in cat_col:
-                df_encoded[col]=label.fit_transform(df[col])
+        df_out=create_cluster(df,n=3,features=features)
+       
 
-    except:
-        st.error("Encoding Error")
+        df_out_with_iq=calculate_iq(df_out)
 
+        st.subheader("📊 Processed Data with Placement Clusters:")
+        st.dataframe(df_out_with_iq[['Cluster', 'PlacementChance','IQ'] + features].head())
+
+        flag=True
+        
             
-    try:        
-            df_scaled=df_encoded[features]
-            nu_col=df_scaled.select_dtypes(include=['float64','int64']).columns
-            scaler=StandardScaler()
-            df_scaled[nu_col]=scaler.fit_transform(df_encoded[nu_col])    
-    except:
-        st.error("Scaling Error")
-
-    try:
-            km=KMeans(n_clusters=3,random_state=42)
-            X=df_scaled
-            df_encoded['cluster']=km.fit_predict(X)
-
-            map = { 0:'Medium',
-            1:'Low',
-            2:'High'}
             
-            df_encoded['PlacementChance']=df['PlacementChance']=df_encoded['cluster'].apply(lambda x:map.get(x,'unknown'))
-
-            st.subheader("📊 Processed Data with Placement Clusters:")
-            st.dataframe(df_encoded[['cluster', 'PlacementChance'] + features].head())
-            flag=True
-
+        with st.container():
+            col1, col2 = st.columns(2)
+            
             if flag:
-                output_file = df.copy()
-                csv = output_file.to_csv(index=False)
-                st.download_button("📥 Download Clustered Dataset", data=csv, file_name='clustered_placement_data.csv', mime='text/csv')
+                with col1:
+                
+                    output_file = df_out_with_iq.copy()
+                    csv = output_file.to_csv(index=False)
+                    st.download_button("📥 Download Clustered Dataset", data=csv, file_name='clustered_placement_data.csv', mime='text/csv')
+                    
 
+
+                with col2:
+                    on = st.toggle("Show Cluster Graph")
+        
+        
+        if on:
+            with st.spinner("Creating Graph"):
+                    time.sleep(0.3)
+                    plt=plot(df,features=features)
+                    st.title("Cluster Visualization")
+                    st.pyplot(plt)
 
 
 
     except Exception as e:
-            st.error(f"⚠ An error occurred: {str(e)}")
+        st.error(f"⚠ An error occurred: {str(e)}")
